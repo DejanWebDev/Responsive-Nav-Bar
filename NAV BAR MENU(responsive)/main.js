b47f596604e403ae07cae2d493954faab77dd3ff
Dejan Mocevic <89588251+DejanWebDev@users.noreply.github.com>
2021-11-11 20:45:32 +0100

    Add files via upload

---
+++ NAV BAR MENU(responsive)/main.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active"); //if active class exists it will remove it and if it doesnt it will add it
+})
